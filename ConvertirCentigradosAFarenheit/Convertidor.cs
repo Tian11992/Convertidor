3811f9639521ad9a3fce32df718dbfe42da0f9fd
Sebastian Correa <sebas.11992@hotmail.com>
2019-09-22 11:47:48 -0500

    convertidor de grados centigrados a grados fahrenheit

---
+++ ConvertirCentigradosAFarenheit/Convertidor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertirCentigradosAFarenheit
+{
+    public class Convertidor
+    {
+
+        private double centigrados;
+        private double fahrenheit;
+
+        public void setCentigrados(double valor)
+        {
+            this.centigrados = valor;
+        }
+
+        public double getCentigrados()
+        {
+            return this.centigrados;
+        }
+
+        public double convertir()
+        {
+            return ((this.getCentigrados()*9/5)+32);
+        }
+
+    }
+}
---
+++ ConvertirCentigradosAFarenheit/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConvertirCentigradosAFarenheit
+{
+    public partial class Form1 : Form
+    {
+
+        Convertidor convertidor = new Convertidor();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnconvertir_Click(object sender, EventArgs e)
+        {
+            convertidor.setCentigrados(double.Parse(txtconvertir.Text));
+            lblresultado.Text = "El resultado es: " + convertidor.convertir().ToString();
+            lblresultado.Visible = true;
+        }
+    }
+}
